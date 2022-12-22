2741146d4a121188e6460f8062c3a3dd851305a6
Kirk Lauri <klauri@stewleonards.com>
2022-12-22 11:21:04 -0500

    day 02 complete

--- 02/day_02.py
+++ 02/day_02.py
@@ -3,30 +3,27 @@ round_score = 0
 with open('input.txt') as f:
     for round in f.read().split('\n'):
         if round[2:3] == 'X':
-            choice_score += 1
+            round_score += 0
+            if round[0:1] == 'A':
+                choice_score += 3
+            elif round[0:1] == 'B':
+                choice_score += 1
+            else:
+                choice_score += 2
         elif round[2:3] == 'Y':
-            choice_score += 2
+            round_score += 3
+            if round[0:1] == 'A':
+                choice_score += 1
+            elif round[0:1] == 'B':
+                choice_score += 2
+            else:
+                choice_score += 3
         else:
-            choice_score += 3
-        if round[0:1] == 'A':
-            if round[2:3] == 'X':
-                round_score += 3
-            if round[2:3] == 'Y':
-                round_score += 6
-            if round[2:3] == 'Z':
-                round_score += 0
-        if round[0:1] == 'B':
-            if round[2:3] == 'X':
-                round_score += 0
-            if round[2:3] == 'Y':
-                round_score += 3
-            if round[2:3] == 'Z':
-                round_score += 6
-        if round[0:1] == 'C':
-            if round[2:3] == 'X':
-                round_score += 6
-            if round[2:3] == 'Y':
-                round_score += 0
-            if round[2:3] == 'Z':
-                round_score += 3
+            round_score += 6
+            if round[0:1] == 'A':
+                choice_score += 2
+            elif round[0:1] == 'B':
+                choice_score += 3
+            else:
+                choice_score += 1
     print(choice_score + round_score)
